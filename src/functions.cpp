ca144e9d8e50221d14e3c2e93dba887eea687fcc
haoyun0 <847360401@qq.com>
2023-06-06 09:34:37 +0800

    修复46341的bug

--- src/functions.cpp
+++ src/functions.cpp
@@ -294,10 +294,10 @@ double f::linear(int stepMax, int step, double tMax, double tMin) {
     return (tMax - tMin) * (1 - step / (double)stepMax) + tMin;
 }
 double f::t_dist_fast(int stepMax, int step, double tMax, double tMin) {
-    return tMax / (1 + step * step / 300000.0);
+    return tMax / (1 + 1ll * step * step / 300000.0);
 }
 double f::t_dist_slow(int stepMax, int step, double tMax, double tMin) {
-    return tMax / (1 + step * step / 1000000.0);
+    return tMax / (1 + 1ll * step * step / 1000000.0);
 }
 double f::linear_mul(int stepMax, int step, double tMax, double tMin) {
     return tMax / (1 + step / 100000.0);
